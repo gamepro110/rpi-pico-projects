16af5f54a07254bbf2ee7a4e8e7216ae28b3b825
void <k.koelewijn2@gmail.com>
2021-04-23 20:20:49 +0200

    added ssd1306 lib

--- multi-core-example/multiCoreExample.cpp
+++ multi-core-example/multiCoreExample.cpp
@@ -2,6 +2,25 @@
 #include "pico/stdlib.h"
 #include "pico/multicore.h"
 
-int main() {
+void Core1Loop();
+
+int main() { // check why it doenst work
+    stdio_init_all();
+    uint32_t num{0};
+    multicore_launch_core1(Core1Loop);
+
+    while(true) {
+        multicore_fifo_push_blocking(num);
+        printf("num: %d", num);
+        num = multicore_fifo_pop_blocking();
+    }
+
     return 0;
+}
+
+void Core1Loop() {
+    uint32_t in = multicore_fifo_pop_blocking();
+    in++;
+    sleep_ms(200);
+    multicore_fifo_push_blocking(in);
 }
